30144e0306bd69ba74cbe07248c0e3cf7482a6bb
JulAgu <jsagudeloac@unal.edu.co>
2024-01-15 23:07:50 +0100

    evaluator_de_experiences class now fully working

--- scripts/scripts_python/cookie_clusters.py
+++ scripts/scripts_python/cookie_clusters.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 import math
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score
 from sklearn.metrics import calinski_harabasz_score
 
 # mesure de similarité dtw entre deux séries temporelles.
@@ -21,20 +21,20 @@ def create_dic_pixels():
     chaque type d'evolution.
     '''
     pix_foret = [[472, 570], [474, 570], [476, 570], [478, 570], [480, 570],
-                [482, 570], [484, 570], [486, 570], [488, 570]]  # ca change uniformement (saison)
+                 [482, 570], [484, 570], [486, 570], [488, 570]]  # ca change uniformement (saison)
     pix_lac = [[392, 567], [392, 580], [401, 577], [401, 567], [395, 570], 
-            [395, 576], [397, 571], [394, 598], [388, 532]]  # ca change un peu
+               [395, 576], [397, 571], [394, 598], [388, 532]]  # ca change un peu
     pix_apt = [[405, 448], [408, 444], [412, 446], [412, 463], [407, 465],
-            [405, 455], [414, 440], [420, 458], [401, 446]]  # ca change (construction)
+               [405, 455], [414, 440], [420, 458], [401, 446]]  # ca change (construction)
     pix_ensta = [[447, 618], [454, 627], [454, 631], [457, 632], [459, 625],
-                [450, 641], [443, 636], [439, 629], [433, 617]]  # ca change (construction)
+                 [450, 641], [443, 636], [439, 629], [433, 617]]  # ca change (construction)
     pix_agri = [[318, 438], [322, 435], [324, 433], [329, 429], [333, 426],
                 [337, 424], [339, 422], [344, 418], [350, 414]]  # ca peut changer (saison, plantation)
     pix_danone = [[383, 497], [383, 500], [387, 501], [383, 504], [387, 505],
-                [384, 508], [388, 509], [384, 504], [386, 504]]  # ca ne change pas
+                  [384, 508], [388, 509], [384, 504], [386, 504]]  # ca ne change pas
 
-    dic = {'pix_foret':pix_foret, 'pix_lac':pix_lac, 'pix_apt':pix_apt,
-           'pix_ensta':pix_ensta, 'pix_agri':pix_agri, 'pix_danone':pix_danone}
+    dic = {'pix_foret': pix_foret, 'pix_lac': pix_lac, 'pix_apt': pix_apt,
+           'pix_ensta': pix_ensta, 'pix_agri': pix_agri, 'pix_danone': pix_danone}
     list = pix_foret + pix_lac + pix_apt + pix_ensta + pix_agri + pix_danone
 
     return (list, dic)
@@ -60,7 +60,7 @@ def dtw(x, x_prime):
 
 def dtw_matrice(x, centroides):
     '''
-    Fonction que généralise la fonction "dwt" á l’échelle de la matrice.
+    Fonction que généralise la fonction "dwt" á l'échelle de la matrice.
     Retourne la matrice de distances entre tous les elements de la
     matrice de données.
     '''
@@ -108,13 +108,12 @@ def kmeans_dtw(x, k, no_of_iterations):
     return points
 
 
-class evaluator_de_experiences:
+class evaluator_de_experiences(object):
     '''
     Classe qui permet d'evaluer les resultats des differents approches
     de clustering.
     '''
     def __init__(self, yhat, pix_list, pix_dic, matrice) -> None:
-
         self.pix_list = pix_list
         self.pix_dic = pix_dic
         self.yhat = yhat
@@ -136,48 +135,35 @@ class evaluator_de_experiences:
         self.y_hat_clas = new_yhat.to_numpy().squeeze()
         self.y_reel = pd.DataFrame(classes).to_numpy().squeeze()
 
-    def list(self):
+    def show_list(self):
         '''
         Fonction qui affiche les clusters et les pixels qui les composent.
         '''
-        name = ''
-        dico = collections.Counter(yhat)
-        for key in list(dico.keys()):
-            dico[key] = [f'number of vectors = {dico[key]}'] 
-            for index,pos in zip(yhat,range(len(yhat))):
-                if index == key:
-                    if 0<=pos<=8: name = 'pix_danone'
-                    elif 9<=pos<=17: name = 'pix_agri'
-                    elif 18<=pos<=26: name = 'pix_ensta'
-                    elif 27<=pos<=35: name = 'pix_apt'
-                    elif 36<=pos<=44: name = 'pix_lac'
-                    elif 45<=pos<=53: name = 'pix_foret'
-
-                    dico[key].append(f'{pix_interet[pos]}:{name}')
-
+        dico = {}
+        for cluster in set(self.yhat):
+            pixels_in_cluster = np.array(self.pix_list)[self.yhat==cluster].tolist()
+            tmp = np.array(self.y_reel)[self.yhat==cluster].tolist()
+            ett_of_pixels = [list(self.pix_dic.keys())[i] for i in tmp]
+            dico[str(cluster)] = (pixels_in_cluster, ett_of_pixels)
         for key in dico:
             print(f'cluster numero {key}:\n-------------------------------')
-            for part in dico[key]:
-                print(f'{part}')
+            for pixel, ettiquete in zip(dico[key][0], dico[key][1]):
+                print(f'{pixel} : {ettiquete}')
             print('-------------------------------')
 
     def metrics_classif(self):
         '''
         Fonction qui permet de calculer les metrics de classification.
         '''
-        # return pd.DataFrame({'accuracy':accuracy_score(self.y_reel, self.y_hat_classif),
-        #                     'precision':precision_score(self.y_reel, self.y_hat_classif),
-        #                     'recall':recall_score(self.y_reel, self.y_hat_classif),
-        #                     'f1_score':f1_score(self.y_reel, self.y_hat_classif)})
-        # return pd.DataFrame({'accuracy': accuracy_score(self.y_reel, self.y_hat_clas)})
-        return(accuracy_score(self.y_reel, self.y_hat_clas))
+        return pd.DataFrame({'accuracy': [accuracy_score(self.y_reel, self.y_hat_clas)],
+                             'f1_score': [f1_score(self.y_reel, self.y_hat_clas, average = 'macro')]})
 
     def metrics_clustering(self):
         '''
         Fonction qui permet de calculer les metrics de clustering.
         '''
-        # return pd.DataFrame({'calinski_harabasz_score':calinski_harabasz_score(self.matrice, self.yhat)})
-        return calinski_harabasz_score(self.matrice, self.yhat)
+        return pd.DataFrame({'calinski_harabasz_score':
+                             [calinski_harabasz_score(self.matrice, self.yhat)]})
 
     def confusion_matrix(self):
         '''
