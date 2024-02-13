1f5b3d35ed0115e608ec602e55ddd6e0c633b17d
MattVerlynde <matthieu.verlynde@agroparistech.fr>
2024-02-13 15:52:05 +0100

    add buil parameters to find the number of clusters

---
+++ scr/features/test_code2.py
@@ -0,0 +1,13 @@
+from build_parameters import find_num_clusters
+import seaborn as sns
+
+data = sns.load_dataset('iris').iloc[:,0:4]
+
+# Find the optimal number of clusters for the iris dataset
+
+# k = find_num_clusters(data, 1, 10, 'KMedoids', max_iter=300, random_state=42)
+k = find_num_clusters(data, 1, 10, 'KMeans', n_init=10, max_iter=300, random_state=42)
+# k = find_num_clusters(data, 1, 10, 'Hierarchical', method='ward')
+
+print(k)
+
