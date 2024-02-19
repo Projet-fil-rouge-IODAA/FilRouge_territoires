199ae0c832546fa9de00d482c890f2af349212cb
JulAgu <julianstivenagudelo310@gmail.com>
2024-02-19 14:22:25 +0100

    Flake8 modifications

--- models/multiple_cluster_combination_system.py
+++ models/multiple_cluster_combination_system.py
@@ -1,7 +1,6 @@
 # Nada et Julian 06/02/2024
 import random as rd
 import numpy as np
-import random as rd
 
 
 class CollaborativeClustering():
@@ -9,6 +8,15 @@ class CollaborativeClustering():
         self.clusters = args
 
     def fit_predict(self):
+        '''
+        def de matrice :
+        [
+        [[x1,x2], .......... ]
+        [                    ]
+        [                    ]
+
+        ]
+        '''
         n_clusters = []  # on stocke le nombre de clusters pour chaque méthode. len(n_clusters) = nb de méthodes du collaborative clustering
         for i in range(len(self.clusters)):
             n_clusters.append(len(set(self.clusters[i])))
@@ -21,21 +29,23 @@ class CollaborativeClustering():
             while j < len(n_clusters):
                 matrice[sum(n_clusters[1:j])+self.clusters[1][i]][self.clusters[0][i]].append(i)
                 j += 1
-                
+
         # Matrice de confusion
-        confusion = [[len(matrice[j][i]) for i in range(n_clusters[0])] for j in range(sum(n_clusters[1:]))]
+        confusion = [[len(matrice[j][i]) for
+                      i in range(n_clusters[0])] for j in range(sum(n_clusters[1:]))]
         confusion = np.array(confusion).astype(dtype=np.float16)
-        
+
         for i in range(confusion.shape[0]):
             for j in range(confusion.shape[1]):
-                confusion[i, j] = confusion[i, j]/(max(confusion[i, :].sum(), (confusion[:, j].sum()/(max(n_clusters[1:])-1))))  # max(n_vert,n_bleu) ou n_rouge??
+                confusion[i, j] = confusion[i, j]/(max(confusion[i, :].sum(),
+                                                       (confusion[:, j].sum()/(max(n_clusters[1:])-1))))  # max(n_vert,n_bleu) ou n_rouge??
 
         # Construction du vecteur clusters
         vect_cluster = [0]*(sum(n_clusters[1:]))
         for i in range(confusion.shape[0]):
             vect_cluster[i] = np.argmax(confusion[i,:])
-        
-        # to relabel clustering results
+
+        # To relabel clustering results
         clusters_relabeled = self.clusters
         clusters_relabeled[1] = np.where(self.clusters[j] == k, vect_cluster[k], clusters_relabeled[j])
         j = 2
@@ -43,7 +53,7 @@ class CollaborativeClustering():
             for k in range(0, n_clusters[j]):
                 clusters_relabeled[j] = np.where(self.clusters[j] == k, vect_cluster[k+sum(n_clusters[1:j-1])], clusters_relabeled[j])
             j += 1
-        
+
         # Matrice de clusterings "relabeled"
         results = np.column_stack(clusters_relabeled)  # attention : cases NaN si les listes clusters ne sont pas de la même taille. np.unique va les ignorer.
 
