56462befe5868aa76e4da9fd4e11abd4370866de
Nada Kassara <kassaranada@gmail.com>
2024-02-20 19:55:08 +0100

    modif scripts

--- models/multiple_cluster_combination_system_test.py
+++ models/multiple_cluster_combination_system_test.py
@@ -10,13 +10,12 @@ class CollaborativeClustering():
     Ensemble de modèles de collaborative clustering
     '''''
 
-    def __init__(self, *args, clusters = [], n_bandes = 3):
+    def __init__(self, *args, clusters = [], modele = AgglomerativeClustering, n_clusters = 4):
         self.pixels = args
-        self.modele = AgglomerativeClustering(n_clusters = 4)
-        self.modele1 = AgglomerativeClustering(n_clusters = 4)
-        self.modele2 = AgglomerativeClustering(n_clusters = 4)
+        self.modele = modele(n_clusters)
         self.clusters = clusters
-        self.n_bandes = n_bandes
+        self.n_bandes = len(self.pixels)
+        self.n_clusters = n_clusters
 
     def initial_clustering(self):
         '''''
@@ -28,18 +27,10 @@ class CollaborativeClustering():
         ]
         '''''
         
-        # for i in range(self.n_bandes):
-        #     self.modele.fit(self.pixels[i])
-        #     self.clusters.append(self.modele.labels_.tolist())
+        for i in range(self.n_bandes):
+            self.modele.fit(self.pixels[i])
+            self.clusters.append(self.modele.labels_.tolist())
 
-        self.modele.fit(self.pixels[0])
-        self.clusters.append(self.modele.labels_.tolist())
-
-        self.modele1.fit(self.pixels[1])
-        self.clusters.append(self.modele1.labels_.tolist())
-
-        self.modele2.fit(self.pixels[2])
-        self.clusters.append(self.modele2.labels_.tolist())
         return self.clusters
 
     def iccm(self):
@@ -48,18 +39,20 @@ class CollaborativeClustering():
         Modèle de collaborative clustering qui prend en entrée la liste des clusters pour chaque méthode qui vote.
         Le modèle renvoie une unique liste de clusters. 
         '''''
-        matrice = [[[] for _ in range(0, 4)] for _ in range(4*(self.n_bandes-1))]
+        matrice = [[[] for _ in range(0, self.n_clusters)] for _ in range(self.n_clusters*(self.n_bandes-1))]
+
         for i in range(len(self.clusters[0])):
-            matrice[self.clusters[1][i]][self.clusters[0][i]].append(i)
-            matrice[4+self.clusters[2][i]][self.clusters[0][i]].append(i)
+            for j in range(self.n_bandes-1):
+                matrice[(4*j)+self.clusters[1+j][i]][self.clusters[0][i]].append(i)
+                # matrice[4+self.clusters[2][i]][self.clusters[0][i]].append(i)
 
         # Matrice de confusion
-        confusion = [[len(matrice[j][i]) for i in range(4)] for j in range(4*(self.n_bandes-1))]
+        confusion = [[len(matrice[j][i]) for i in range(self.n_clusters)] for j in range(self.n_clusters*(self.n_bandes-1))]
         confusion = np.array(confusion).astype(dtype=np.float16)
 
         for i in range(confusion.shape[0]):
             for j in range(confusion.shape[1]):
-                confusion[i, j] = confusion[i, j]/(max(confusion[i, :].sum(),(confusion[:, j].sum()/2)))
+                confusion[i, j] = confusion[i, j]/(max(confusion[i, :].sum(), confusion[:, j].sum())/(self.n_bandes-1))
 
         # Construction du vecteur clusters
         vect_cluster = [0]*(4*(self.n_bandes-1))
@@ -70,7 +63,7 @@ class CollaborativeClustering():
         clusters_relabeled = list(self.clusters)
         j = 1
         while j < self.n_bandes:
-            for k in range(0, 4):
+            for k in range(0, self.n_clusters):
                 clusters_relabeled[j] = np.where(self.clusters[j] == k, vect_cluster[k+4*(j-1)], clusters_relabeled[j])
             j += 1
 
@@ -82,8 +75,8 @@ class CollaborativeClustering():
         for i in range(len(self.clusters[0])):
             values, counts = np.unique(results[i, :], return_counts=True)
             if np.all(counts == 1):
-                final_clusters.append(rd.randint(0, 4))
+                final_clusters.append(rd.randint(0, self.n_clusters))
             else:
                 final_clusters.append(values[counts.argmax()])
-        
-        return final_clusters
+      
+        return np.array(final_clusters)
