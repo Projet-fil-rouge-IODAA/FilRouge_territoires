78b6b726f534f03e649cf24684bbc8e987fe8f94
JulAgu <julianstivenagudelo310@gmail.com>
2024-02-20 16:49:09 +0100

    Changes in images generation and app

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit_image_comparison import image_comparison
+from streamlit_cropper import st_cropper
+from PIL import Image
 
 st.set_page_config(
     page_title="Evolution de territoires",
@@ -7,19 +9,25 @@ st.set_page_config(
     layout="wide",
 )
 
+
 def main():
     """
     The main function
     """
     # Main page
-    st.title("Caractérisation de l'évolution du territoire 🗺️")
+    col01, col02 = st.columns([0.8, 0.2], gap='medium')
+    with col01:
+        st.title("Caractérisation de l'évolution du territoire 🗺️")
+    with col02:
+        st.image("../app/src/Logo_AgroParisTech.png", width=200)
+
     st.header("Un approache par clustering de series temporelles.")
     st.subheader("Projet fil Rouge - IODAA 2024.")
     st.markdown("**Auteurs**: Afonso Ponce, Julian Agudelo, \
                 Matthieu Verlynde, \
                 Nada Kassara, Malek Baroudi.")
     # Introduction
-    st.subheader("Introduction")
+    st.header("Introduction")
     st.write("Cette application est un moyen simple de \
              visualiser et d'utiliser \
              les deux principales méthodologies qui ont été \
@@ -41,29 +49,52 @@ def main():
              qu'il souhaite générer.")
 
     # Utilisez les modèles
-    st.subheader("Utilisez les modèles")
-    
-    st.slider("Sélectionnez le nombre de clusters: ", min_value=0, max_value=40)
+    st.header("Utilisez les modèles")
+    col1, col2 = st.columns([1.1, 1], gap='small')
 
-    st.selectbox('Sélectionnez la méthodologie à utiliser: ',
-                 ["Time2feat", "Combinaison de clusterings"])
+    with col1:
+        st.subheader("Choisissez un région d'interet")
+        img = Image.open("../results/crop_image.png")
+        cropped_img = st_cropper(img, realtime_update=True, box_color='#0000FF',
+                                 aspect_ratio=None)
 
-    st.button("Lancer")
+        col11, col12 = st.columns(2, gap = 'small')
+        with col11:
+            _ = cropped_img.thumbnail((400, 200))
+            st.image(cropped_img)
+        with col12:
+            st.write("")
+            st.write(f"Taille originale: {img.size[0]}px, {img.size[1]}px")
+            st.write(f"Taille de votre selection: {cropped_img.size[0]}px, {cropped_img.size[1]}px")
+            st.write(f"Nombre de pixels dans votre selection: {cropped_img.size[0]*cropped_img.size[1]}")
 
-    st.subheader("Visualisez et analysez les résultats")
+    with col2:
+        st.subheader("Choisissez le nombre de clusters et la méthodologie")
+        n_clust = st.selectbox('Sélectionnez le nombre de clusters: ',
+                            ["Automatique", "Personnalisé"])
+        if n_clust == "Personnalisé":
+            st.slider("", min_value=0, max_value=40)
 
-    # Visualisation des résultats
-    image_comparison(
-    img1="../results/base_image.jpg",
-    img2="../results/cluster_results.jpg",
-    label1="Image originale",
-    label2="Image clusterisée",
-    width=1000,
-    starting_position=50,
-    show_labels=True,
-    make_responsive=True,
-    in_memory=True,
-    )
+        st.selectbox('Sélectionnez la méthodologie à utiliser: ',
+                    ["Time2feat", "Combinaison de clusterings"])
+
+        executer = st.button("Exécuter")
+
+    if executer:
+        st.subheader("Visualisez et analysez les résultats")
+
+        # Visualisation des résultats
+        image_comparison(
+        img1="../results/base_image.jpg",
+        img2="../results/cluster_results.jpg",
+        label1="Image originale",
+        label2="Image clusterisée",
+        width=1000,
+        starting_position=50,
+        show_labels=True,
+        make_responsive=True,
+        in_memory=True,
+        )
 
 
 if __name__ == "__main__":
